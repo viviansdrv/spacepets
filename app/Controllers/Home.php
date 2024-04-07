a9a065b290fc2ba377c55ae79bc1cc473ce69c50
viviansdrv <83466012+viviansdrv@users.noreply.github.com>
2024-04-07 19:36:44 -0300

    Home changed

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,10 +4,19 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    /* public function index(): string
     {
-        return view('welcome_message');
+        return view('welcome_message'); //antigo (padrão)
         // em vez de mexer na rota, poderia colocar assim:'
-        // return redirect()->to('/login');
-    }
+        //return redirect()->to('/login');
+    } */ 
+
+    
+    public function index()
+    {
+        //return view('welcome_message'); antigo (padrão)
+        // em vez de mexer na rota, poderia colocar assim:'
+        return redirect()->to('/login');
+    } 
+
 }
